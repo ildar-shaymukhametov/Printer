634aa23b9b13ee547e3e4583d730af9d7fb881eb
Шаймухаметов Ильдар <shaimildar@gmail.com>
2023-08-22 21:56:45 +0500

    add program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿await ProcessRequest(new Request
+{
+    Pages = 100
+});
+
+async Task Print(int printerNo, int pages)
+{
+    foreach (var page in Enumerable.Range(1, pages))
+    {
+        await Task.Delay(250);
+        Console.WriteLine($"Printer {printerNo} prints page #{page}");
+    }
+
+    Console.WriteLine($"Printer {printerNo} is done");
+}
+
+async Task ProcessRequest(Request request)
+{
+    var printers = new[] { 1, 2, 3, 4 };
+    var tasks = printers.Select(printerNo => Print(printerNo, request.Pages / printers.Length));
+    await Task.WhenAll(tasks);
+}
+
+class Request
+{
+    public int Pages { get; set; }
+}
+
